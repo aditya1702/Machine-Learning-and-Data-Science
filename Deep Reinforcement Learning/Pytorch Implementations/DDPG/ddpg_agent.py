426409faf908c4b914e4c1ce8ed33e76b3eb3d41
Aditya Vyas <adityavyas17@gmail.com>
2018-02-20 01:10:14 +0530

    Make changes

--- Deep Reinforcement Learning/Pytorch Implementations/DDPG/ddpg_agent.py
+++ Deep Reinforcement Learning/Pytorch Implementations/DDPG/ddpg_agent.py
@@ -90,30 +90,27 @@ class Ddpg:
         for episode_number in range(self.number_of_episodes):
             self.total_reward_gained = 0
             self.current_episode_number = episode_number
-            self.run_episode(rl_environment)
+
+            state = rl_environment.reset()
+            while True:
+                action = self._select_action(state)
+                next_state, reward, done, info = rl_environment.step(action)
+
+                self.total_reward_gained += reward
+                print(self.total_reward_gained)
+
+                self.replay_memory.append(self.Transition(state, action, next_state, reward, done))
+
+                self._update_network_parameters()
+
+                if done:
+                    self._save_reward_info(reward=self.total_reward_gained)
+                    break
+                state = next_state
             print(self.total_reward_gained)
 
         if self.plot_environment_statistics:
             self._plot_environment_statistics()
-
-    def run_episode(self, rl_environment):
-
-        state = rl_environment.reset()
-        while True:
-            action = self._select_action(state)
-            next_state, reward, done, info = rl_environment.step(action)
-
-            self.total_reward_gained += reward
-            print(self.total_reward_gained)
-
-            self.replay_memory.append(self.Transition(state, action, next_state, reward, done))
-
-            self._update_network_parameters()
-
-            if done:
-                self._save_reward_info(reward = self.total_reward_gained)
-                break
-            state = next_state
 
     def _select_action(self, state):
         state_tensor = self.utils.numpy_array_to_torch_tensor(np.array(state))
